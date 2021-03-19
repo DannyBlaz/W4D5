6e3561d335877a2d4fa52df2f38a11edd5ed555f
Robby Balistreri <rbalistreri5@gmail.com>
2021-03-19 14:51:18 -0500

    Add anagrams part 3

--- Anagrams.rb
+++ Anagrams.rb
@@ -19,3 +19,36 @@ end
 
 p first_anagram?("gizmo", "sally")    #=> false
 p first_anagram?("elvis", "lives")    #=> true
+
+def second_anagram?(str1, str2)
+    str1.each_char do |char|
+        return false if !str2.include?(char)
+        str2[str2.index(char)] = ''
+    end
+    str2.empty?
+end 
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+def third_anagram?(str1, str2)
+    is_sorted(str1) == is_sorted(str2)   
+end
+
+def is_sorted(string)
+    alphabet = ('a'..'z').to_a
+    sorted = false
+    until sorted
+        sorted = true
+        (0...string.length-1).each do |idx|
+            if alphabet.index(string[idx]) > alphabet.index(string[idx + 1])
+                string[idx], string[idx + 1] = string[idx+1], string[idx]
+                sorted = false
+            end
+        end
+    end
+    string
+end
+
+ p second_anagram?("gizmo", "sally")    #=> false
+ p second_anagram?("elvis", "lives")    #=> true
