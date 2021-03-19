2e3fe87fb831aebe56ac9294ec226f21d0af9f9f
Daniel <dannyblaze469@gmail.com>
2021-03-19 17:32:19 -0400

    Finished Two sum

--- Two_sum.rb
+++ Two_sum.rb
@@ -1,10 +1,10 @@
 require "byebug"
 def two_sum_v1?(arr, target)
 
-    arr.each_with_index do |ele1, idx1|
-        arr.each_with_index do |ele2, idx2|
+    arr.each_with_index do |ele1, idx1| #o(n)
+        arr.each_with_index do |ele2, idx2| #o(n)
             if idx1 < idx2
-                return true if ele1 + ele2 == target
+                return true if ele1 + ele2 == target 
             end
         end
     end
@@ -15,18 +15,18 @@ arr = [0, 1, 5, 7]
 
 
 
-def two_sum_v2?(arr,target)
-sorted = quick_sort(arr)
-    sorted.each_with_index do |ele, idx|
+def two_sum_v2?(arr,target) # linearithmic 
+sorted = quick_sort(arr) #o(nlogn)
+    sorted.each_with_index do |ele, idx| #o(n)
         # debugger
         search_value = target - ele
-        search_index = bsearch(sorted, search_value)
+        search_index = bsearch(sorted, search_value) # o(logn)
         return true if (idx != search_index && search_index != nil)
     end  
 false
 end
 
-def quick_sort(arr)
+def quick_sort(arr) # linearthmic
     return arr if arr.length <= 1
     pivot = arr.first
     left_side = arr[1..-1].select {|num| num < pivot}
@@ -34,7 +34,7 @@ def quick_sort(arr)
     quick_sort(left_side) + [pivot] + quick_sort(right_side)
 end
 
-def bsearch(arr, target)
+def bsearch(arr, target) #logarithmic
     mid = arr.length / 2
     case target <=> arr[mid]
     when -1
@@ -56,5 +56,19 @@ end
 # arr = [1,2,3,4,5,6,7,8,9]
 # p bsearch(arr, 6)
 
-p two_sum_v2?(arr, 6) # => should be true
-p two_sum_v2?(arr, 11) # => should be false
+# p two_sum_v2?(arr, 6) # => should be true
+# p two_sum_v2?(arr, 11) # => should be false
+
+
+def two_sum_v3?(arr, target) 
+    hash = Hash.new
+    arr.each { |ele| hash[ele] = ele } #o(n)
+    arr.each do |ele| #o(n)
+        search_value = target - ele
+        return true if !hash[search_value].nil? && hash[search_value] != ele
+    end
+    false
+end
+
+p two_sum_v3?(arr, 6) # => should be true
+p two_sum_v3?(arr, 10) # => should be false
